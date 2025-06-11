6da8acd359e834cc4cdf0a24c8457efbec4f1b10
Kauã Pereira <zkauacodes@hotmail.com>
2025-06-11 11:37:06 -0300

    refactor(create-team-usecase): added call create team in github, in future refactor team and user to receive/create github user with mail and save user name to use in this usecase

--- application/src/main/java/com/kaua/devkit/platform/application/usecases/teams/create/CreateTeamInput.java
+++ application/src/main/java/com/kaua/devkit/platform/application/usecases/teams/create/CreateTeamInput.java
@@ -1,10 +1,11 @@
 package com.kaua.devkit.platform.application.usecases.teams.create;
 
 public record CreateTeamInput(
-        String teamName
+        String teamName,
+        String ownerId
 ) {
 
-    public static CreateTeamInput with(final String teamName) {
-        return new CreateTeamInput(teamName);
+    public static CreateTeamInput with(final String teamName, final String ownerId) {
+        return new CreateTeamInput(teamName, ownerId);
     }
 }
--- application/src/main/java/com/kaua/devkit/platform/application/usecases/teams/create/DefaultCreateTeamUseCase.java
+++ application/src/main/java/com/kaua/devkit/platform/application/usecases/teams/create/DefaultCreateTeamUseCase.java
@@ -1,19 +1,38 @@
 package com.kaua.devkit.platform.application.usecases.teams.create;
 
 import com.kaua.devkit.platform.application.exceptions.UseCaseInputCannotBeNullException;
+import com.kaua.devkit.platform.application.gateways.TemplateGateway;
+import com.kaua.devkit.platform.application.repositories.TeamMemberRepository;
 import com.kaua.devkit.platform.application.repositories.TeamRepository;
+import com.kaua.devkit.platform.application.repositories.UserRepository;
 import com.kaua.devkit.platform.domain.exceptions.DomainException;
+import com.kaua.devkit.platform.domain.exceptions.NotFoundException;
 import com.kaua.devkit.platform.domain.teams.Team;
+import com.kaua.devkit.platform.domain.teams.TeamMember;
+import com.kaua.devkit.platform.domain.teams.TeamMemberRole;
+import com.kaua.devkit.platform.domain.users.User;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 public class DefaultCreateTeamUseCase extends CreateTeamUseCase {
 
     private final TeamRepository teamRepository;
+    private final TeamMemberRepository teamMemberRepository;
+    private final UserRepository userRepository;
+    private final TemplateGateway templateGateway;
 
-    public DefaultCreateTeamUseCase(final TeamRepository teamRepository) {
+    public DefaultCreateTeamUseCase(
+            final TeamRepository teamRepository,
+            final TeamMemberRepository teamMemberRepository,
+            final UserRepository userRepository,
+            final TemplateGateway templateGateway
+    ) {
         this.teamRepository = Objects.requireNonNull(teamRepository);
+        this.teamMemberRepository = Objects.requireNonNull(teamMemberRepository);
+        this.userRepository = Objects.requireNonNull(userRepository);
+        this.templateGateway = Objects.requireNonNull(templateGateway);
     }
 
     @Override
@@ -24,12 +43,27 @@ public class DefaultCreateTeamUseCase extends CreateTeamUseCase {
             throw DomainException.with("Team already exists with this name"); // TODO change to custom exception
         }
 
+        final var aUser = this.userRepository.userOfId(input.ownerId())
+                .orElseThrow(NotFoundException.with(User.class, input.ownerId()));
+
+        final var aTeamMember = TeamMember.newMember(
+                aUser.getId(),
+                TeamMemberRole.TECHLEAD
+        );
+
         final var aTeam = Team.newTeam(
                 input.teamName(),
-                new ArrayList<>()
+                new ArrayList<>(Collections.singleton(aTeamMember.getId()))
         );
 
         this.teamRepository.save(aTeam);
+        this.teamMemberRepository.save(aTeamMember);
+
+        // TODO in future get github name
+        this.templateGateway.createTeam(new TemplateGateway.CreateTeamRequest(
+                input.teamName(),
+                Collections.singletonList(aUser.getName().firstName())
+        ));
 
         return CreateTeamOutput.from(aTeam);
     }
